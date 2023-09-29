257a44c0893f98e559b3db93ca78613021669fc7
Gabriel Pimenta <gabriel.pimenta@sistemairis.com.br>
2023-09-29 16:02:29 -0300

    ➕ add dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
